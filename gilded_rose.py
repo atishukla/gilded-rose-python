9551ab286702d460405b9a577ab22bf5c98ec09f
atishayshukla <atishayshukla@gmail.com>
2019-09-08 13:54:59 +0200

    Using class variables for the entities

--- gilded_rose.py
+++ gilded_rose.py
@@ -19,20 +19,20 @@ class GildedRose(object):
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.name == self.backstage:
                         if item.sell_in < 11:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+            if item.name != self.sulfuras:
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item.name != self.aged_brie:
+                    if item.name != self.backstage:
                         if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
+                            if item.name != self.sulfuras:
                                 item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
