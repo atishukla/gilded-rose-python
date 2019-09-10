2bcf3cfb2e0a246be7a509e05cc52f1e78a24ccf
atishayshukla <atishayshukla@gmail.com>
2019-09-10 22:02:41 +0200

    created invert if condition for backstage

--- gilded_rose.py
+++ gilded_rose.py
@@ -34,13 +34,12 @@ class GildedRose(object):
                 if item.name == self.aged_brie:
                     if item.quality < 50:
                         item.quality = item.quality + 1
-
-                elif item.name != self.backstage:
-                    if item.quality > 0:
-                        if item.name != self.sulfuras:
-                            item.quality = item.quality - 1
-                else:
+                elif item.name == self.backstage:
                     item.quality = item.quality - item.quality
+
+                elif item.quality > 0:
+                    if item.name != self.sulfuras:
+                        item.quality = item.quality - 1
 
 
 class Item:
